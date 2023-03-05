71dda6bb1a583076a99e05ae2f5ff1d9d4d8d495
Aditya Winata <aditya@pop-os.localdomain>
2023-03-05 21:52:54 +0700

    added tailwindcss, footer Frontend

--- bhfms-app/public/js/app.js
+++ bhfms-app/public/js/app.js
@@ -25730,12 +25730,12 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
 /* harmony import */ var _welcome_vue_vue_type_template_id_f8506a2e__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./welcome.vue?vue&type=template&id=f8506a2e */ "./resources/js/Pages/welcome.vue?vue&type=template&id=f8506a2e");
-/* harmony import */ var _mnt_d_Microsoft_Project_BINUS_University_Semester_8_Skripsi_BHFMS_bhfms_app_node_modules_vue_loader_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./node_modules/vue-loader/dist/exportHelper.js */ "./node_modules/vue-loader/dist/exportHelper.js");
+/* harmony import */ var _home_aditya_Documents_BINUS_Skripsi_BHFMS_bhfms_app_node_modules_vue_loader_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./node_modules/vue-loader/dist/exportHelper.js */ "./node_modules/vue-loader/dist/exportHelper.js");
 
 const script = {}
 
 ;
-const __exports__ = /*#__PURE__*/(0,_mnt_d_Microsoft_Project_BINUS_University_Semester_8_Skripsi_BHFMS_bhfms_app_node_modules_vue_loader_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_1__["default"])(script, [['render',_welcome_vue_vue_type_template_id_f8506a2e__WEBPACK_IMPORTED_MODULE_0__.render],['__file',"resources/js/Pages/welcome.vue"]])
+const __exports__ = /*#__PURE__*/(0,_home_aditya_Documents_BINUS_Skripsi_BHFMS_bhfms_app_node_modules_vue_loader_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_1__["default"])(script, [['render',_welcome_vue_vue_type_template_id_f8506a2e__WEBPACK_IMPORTED_MODULE_0__.render],['__file',"resources/js/Pages/welcome.vue"]])
 /* hot reload */
 if (false) {}
 
--- bhfms-app/resources/js/Pages/welcome.vue
+++ bhfms-app/resources/js/Pages/welcome.vue
@@ -1,7 +1,11 @@
 <script setup>
+ import Footer from "../Shared/Footer.vue";
 
 </script>
 
 <template>
     hello, welcome to inertia
+    <Footer>
+
+    </Footer>
 </template>
