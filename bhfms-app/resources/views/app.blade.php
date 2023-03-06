e8b8032bfc15ad67ce1a26ff93637861df2973b5
AndreanIG <andrean.2000@gmail.com>
2023-03-06 15:47:35 +0700

    All Database Table

--- bhfms-app/resources/views/app.blade.php
+++ bhfms-app/resources/views/app.blade.php
@@ -3,12 +3,6 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-<<<<<<< HEAD
-=======
-    <link href="https://cdn.jsdelivr.net/npm/daisyui@2.13.6/dist/full.css" rel="stylesheet" type="text/css" />
-    <script src="https://cdn.tailwindcss.com"></script>
-    <link rel="stylesheet" href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css">
->>>>>>> parent of c93580b (Add Models)
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
     <script src="{{ mix('/js/app.js') }}" defer></script>
     @inertiaHead
