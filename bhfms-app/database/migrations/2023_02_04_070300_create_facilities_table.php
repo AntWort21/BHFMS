f44ba938273a7ba8730c5c8eb0e05dd573a0db44
AndreanIG <andrean.2000@gmail.com>
2023-03-07 11:13:13 +0700

    Little update

--- bhfms-app/database/migrations/2023_02_04_070300_create_facilities_table.php
+++ bhfms-app/database/migrations/2023_02_04_070300_create_facilities_table.php
@@ -16,7 +16,6 @@ class CreateFacilitiesTable extends Migration
         Schema::create('facility_details', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('description');
             $table->timestamps();
         });
     }
