c93580b0876ff024c8492f33104d8376b486c297
AndreanIG <andrean.2000@gmail.com>
2023-03-09 20:21:54 +0700

    Add Models

--- bhfms-app/routes/web.php
+++ bhfms-app/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BoardingController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return inertia('welcome');
 });
+
+Route::resource('boarding', BoardingController::class);
