478bbdc44598692a98ed8304f6fb9c71890cbc1a
GardionS <kanbeisensei@gmail.com>
2023-03-19 22:03:43 +0700

    Add Models
    Add Models

---
+++ bhfms-app/app/Models/boarding_house_types.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class boarding_house_types extends Model
+{
+    use HasFactory;
+}
