4b28f3dabe4be9edd4db5d75956799a3edea58f4
AndreanIG <andrean.2000@gmail.com>
2023-03-26 20:24:58 +0700

    Update Migrations

---
+++ bhfms-app/database/migrations/2023_03_06_084028_create_owner_boarding_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOwnerBoardingTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('owner_boardings', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')
+            ->on('users')->cascadeOnUpdate()->cascadeOnDelete();
+
+            $table->unsignedBigInteger('boarding_id');
+            $table->foreign('boarding_id')->references('id')
+            ->on('boardings')->cascadeOnUpdate()->cascadeOnDelete();
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('owner_boardings');
+    }
+}
