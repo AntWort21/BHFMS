bcbe6c6828c728026f517aabe78ec8acf0ddbb3b
AntWort21 <67149821+AntWort21@users.noreply.github.com>
2023-03-26 21:52:33 +0700

    edit profile, only needs background image

--- bhfms-app/resources/js/Pages/EditProfile.vue
+++ bhfms-app/resources/js/Pages/EditProfile.vue
@@ -3,6 +3,7 @@ import Header from "../Shared/Header.vue";
 import Footer from "../Shared/Footer.vue";
 import FormTextBoxInput from "../Shared/FormTextBoxInput.vue";
 import { useForm } from "@inertiajs/inertia-vue3";
+import { Inertia } from "@inertiajs/inertia";
 
 const props = defineProps({
     userDetails: Object,
@@ -13,6 +14,7 @@ let form = useForm({
     email: props.userDetails.email,
     dateOfBirth: props.userDetails.date_of_birth,
     phoneNumber: props.userDetails.phone,
+    profilePicture: props.userDetails.profile_picture,
 });
 
 let submit = () => {
@@ -22,17 +24,17 @@ let submit = () => {
 
 <template>
     <Header />
-    <section class="w-full max-h-screen h-[80vh] flex justify-center bg-black">
+    <section class="w-full max-h-screen h-[88vh] flex justify-center bg-black">
         <div
             class="flex my-5 ml-10 mr-5 bg-white w-1/6 h-2/5 justify-center items-center"
         >
-            <img src="" alt="No Image" />
+            <img :src="userDetails.profile_picture" alt="No Image" class="h-full w-full"/>
         </div>
         <div class="flex flex-col my-5 ml-5 mr-10 bg-white w-1/2 h-2/3">
             <div class="text-2xl mt-4 mx-4 text-indigo-700">
                 Profile Account
             </div>
-            <form @submit.prevent="submit">
+            <form @submit.prevent="submit" enctype="multipart/form-data">
                 <div class="flex flex-col grow mx-4 space-y-2">
                     <FormTextBoxInput
                         v-model="form.name"
@@ -55,6 +57,12 @@ let submit = () => {
                         :input-type="'text'"
                         :label-name="'Phone Number'"
                     />
+                    <label class="block">Profile Picture Image</label>
+                    <input
+                        type="file"
+                        @input="form.profilePicture = $event.target.files[0]"
+                        class="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
+                    />
                     <div
                         class="flex items-baseline justify-betweens space-x-6 mt-auto mb-10 mx-2"
                     >
@@ -65,7 +73,7 @@ let submit = () => {
                         </button>
                         <button
                             type="reset"
-                            class="px-6 py-2 mt-4 text-white bg-red-600 rounded-lg hover:bg-blue-900"
+                            class="px-6 py-2 mt-4 text-white bg-red-600 rounded-lg hover:bg-red-900"
                         >
                             Reset
                         </button>
