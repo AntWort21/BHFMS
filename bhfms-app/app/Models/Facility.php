8f19a61f13dc628e2f6289c8e9efed7a11d251f5
AndreanIG <andrean.2000@gmail.com>
2023-03-28 20:14:30 +0700

    Resolve Deleted File

---
+++ bhfms-app/app/Models/Facility.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Facility extends Model
+{
+    use HasFactory;
+
+    public function facilityDetail(){
+        return $this->belongsTo(FacilityDetail::class,'facility_details_id');
+    }
+
+    public function boarding(){
+        return $this->belongsTo(Boarding::class,'boardings_id');
+    }
+}
