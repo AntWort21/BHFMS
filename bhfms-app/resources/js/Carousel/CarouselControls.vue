1680b1d594a65a8033acece3ca0d750c9d3e5e84
AndreanIG <andrean.2000@gmail.com>
2023-03-28 20:17:00 +0700

    Add carousel Component (#27)
    * All Database Table
    
    * npm run dev
    
    * Add Models
    
    * Facility Starting
    
    * Delete Previous Database
    
    * Update Migrations
    
    * Add Carousel
    
    * Resolve Review
    
    * Resolve Deleted File

---
+++ bhfms-app/resources/js/Carousel/CarouselControls.vue
@@ -0,0 +1,23 @@
+<template>
+    <!-- in vue 2.x you need wrap these buttons in a div -->
+    <button class="carousel-control left" @click="$emit('prev')">Prev</button>
+    <button class="carousel-control right" @click="$emit('next')">Next</button>
+</template>
+
+<script>
+export default {
+    emits: ["prev", "next"], // vue v3
+    props: ["size"],
+};
+</script>
+<style scoped>
+.carousel-control {
+    @apply bg-[rgba(0,0,0,0.5)] absolute h-[10%] w-[10%] align-middle text-[#f3f3f3] cursor-pointer border-[none] top-[40%];
+}
+.left {
+    @apply left-0;
+}
+.right {
+    @apply right-0;
+}
+</style>
---
+++ bhfms-app/resources/js/Carousel/CarouselItem.vue
@@ -0,0 +1,55 @@
+<template>
+    <transition :name="transitionEffect">
+        <div
+            class="carousel-item"
+            v-show="currentSlide === index"
+            @mouseenter="$emit('mouseenter')"
+            @mouseout="$emit('mouseout')"
+        >
+            <img :src="slide" />
+        </div>
+    </transition>
+</template>
+
+<script>
+export default {
+    emits: ["mouseenter", "mouseout"],
+    props: ["slide", "currentSlide", "index", "direction"],
+    computed: {
+        transitionEffect() {
+            return this.direction === "right" ? "slide-out" : "slide-in";
+        },
+    },
+};
+</script>
+
+<style scoped>
+.carousel-item {
+    @apply w-full h-full absolute inset-0;
+    /* align-items: center; */
+}
+.slide-in-enter-active,
+.slide-in-leave-active,
+.slide-out-enter-active,
+.slide-out-leave-active {
+    @apply transition-all duration-[1s] ease-[ease-in-out];
+}
+.slide-in-enter-from {
+    @apply -translate-x-full;
+}
+.slide-in-leave-to {
+    @apply translate-x-full;
+}
+.slide-out-enter-from {
+    @apply translate-x-full;
+}
+.slide-out-leave-to {
+    @apply -translate-x-full;
+}
+img {
+    @apply w-full h-full;
+    /* position: absolute; */
+    /* max-width: 100%; */
+    /* height: auto; */
+}
+</style>
