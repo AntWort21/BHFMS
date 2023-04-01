4d81e996f42fa4bb9f20bb773ac122e73708c094
AndreanIG <andrean.2000@gmail.com>
2023-04-01 18:48:18 +0700

    Added Location Migraton, Seeder & Model (#31)

---
+++ bhfms-app/app/Models/District.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class District extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'id',
+        'name',
+        'city_id',
+        'lat',
+        'lng',
+    ];
+    public function country(){
+        return $this->belongsTo(City::class,'cities_id');
+    }
+}
