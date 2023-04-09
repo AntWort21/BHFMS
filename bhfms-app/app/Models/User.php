6e9af2bc4945ee04a2bb968488bf531cf04cadd1
Aditya Winata <67149821+AntWort21@users.noreply.github.com>
2023-04-09 20:41:12 +0700

    hotfix fillable user model (#36)

--- bhfms-app/app/Models/User.php
+++ bhfms-app/app/Models/User.php
@@ -39,7 +39,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'user_name',
         'gender',
         'date_of_birth',
         'user_role_id',
