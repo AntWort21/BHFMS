6bdadc97ba0a8378c1544bacb9c259bdc4d0b852
Aditya Winata <67149821+AntWort21@users.noreply.github.com>
2023-04-09 21:43:12 +0700

    Hotfix/seeder (#38)
    * boarding seeder location updated, added user seeder
    
    * boarding seeder location updated, added user seeder, added ownerboarding seeder

--- bhfms-app/database/seeders/DatabaseSeeder.php
+++ bhfms-app/database/seeders/DatabaseSeeder.php
@@ -22,7 +22,9 @@ class DatabaseSeeder extends Seeder
             BoardingSeeder::class,
             FacilitySeeder::class,
             BoardingImageSeeder::class,
+            UserSeeder::class,
+            OwnerBoardingSeeder::class,
         ]);
-        
+
     }
 }
