03bb4fb0f0c055238f06c49a81b602bd4acd7d0e
AndreanIG <andrean.2000@gmail.com>
2023-04-22 18:34:51 +0700

    Updated Fixes

--- bhfms-app/app/Http/Controllers/BoardingController.php
+++ bhfms-app/app/Http/Controllers/BoardingController.php
@@ -13,6 +13,7 @@ use App\Models\ManagerBoarding;
 use App\Models\OwnerBoarding;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use Inertia\Inertia;
 
 class BoardingController extends Controller
@@ -38,6 +39,8 @@ class BoardingController extends Controller
             
         })->paginate(5)->withQueryString();
 
+        // dd($Boarding_data);
+
         return Inertia::render('Boarding/BoardingManagementAdmin', [
             'all_count' => Boarding::count(),
             'approved' => Boarding::join('owner_boardings','boardings.id','=','owner_boardings.boarding_id')->where('status','=','approved')->count(),
@@ -50,10 +53,10 @@ class BoardingController extends Controller
     public function indexManager(Request $request)
     {
         
-        $Boarding_data = Boarding::join('owner_boardings','boardings.id','=','owner_boardings.boarding_id')
-            ->join('manager_boardings','manager_boardings.owner_boarding_id','=','owner_boardings.id')
-            ->join('users','users.id','=','manager_boardings.user_id')
-            ->where('manager_boardings.user_id','=',auth()->id())
+        $Boarding_data = Boarding::join('manager_boardings','manager_boardings.boarding_id','=','boardings.id')
+            ->join('owner_boardings','manager_boardings.owner_boarding_id',"=",'owner_boardings.id')
+            ->join('users','users.id',"=","owner_boardings.user_id")
+            ->where('manager_boardings.manager_boarding_id','=',auth()->id())
             ->when($request->search, function($query, $search){
             if($search=='all'){
                 $query;
@@ -64,11 +67,13 @@ class BoardingController extends Controller
             
         })->paginate(5)->withQueryString();
 
+        // dd($Boarding_data);
+
         return Inertia::render('Boarding/BoardingManagementManager', [
-            'all_count' => ManagerBoarding::where('manager_boardings.user_id','=',auth()->id())->count(),
-            'approved' => ManagerBoarding::join('owner_boardings','manager_boardings.owner_boarding_id','=','owner_boardings.id')->where([['manager_boardings.user_id','=',auth()->id()],['status','=','approved']])->count(),
-            'declined' => ManagerBoarding::join('owner_boardings','manager_boardings.owner_boarding_id','=','owner_boardings.id')->where([['manager_boardings.user_id','=',auth()->id()],['status','=','declined']])->count(),
-            'pending' => ManagerBoarding::join('owner_boardings','manager_boardings.owner_boarding_id','=','owner_boardings.id')->where([['manager_boardings.user_id','=',auth()->id()],['status','=','pending']])->count(),
+            'all_count' => ManagerBoarding::where('manager_boardings.manager_boarding_id','=',auth()->id())->count(),
+            'approved' => ManagerBoarding::join('owner_boardings','manager_boardings.owner_boarding_id','=','owner_boardings.id')->where([['manager_boardings.manager_boarding_id','=',auth()->id()],['status','=','approved']])->count(),
+            'declined' => ManagerBoarding::join('owner_boardings','manager_boardings.owner_boarding_id','=','owner_boardings.id')->where([['manager_boardings.manager_boarding_id','=',auth()->id()],['status','=','declined']])->count(),
+            'pending' => ManagerBoarding::join('owner_boardings','manager_boardings.owner_boarding_id','=','owner_boardings.id')->where([['manager_boardings.manager_boarding_id','=',auth()->id()],['status','=','pending']])->count(),
             'boardings' => $Boarding_data,
         ]);
     }
@@ -109,8 +114,7 @@ class BoardingController extends Controller
         return Inertia::render('Boarding/AllBoardingHouse', ['allBoardingHouse' => $allBoardingHouse]);
     }
 
-    //Show the form for creating a new resource.
-    public function createOwnerBoarding()
+    public function getCreateOwnerBoarding()
     {
         $Manager_data = User::where('user_role_id','=','4')->get();
         return Inertia::render('Boarding/CreateBoarding', [
@@ -120,7 +124,7 @@ class BoardingController extends Controller
         ]);
     }
 
-    public function postOwnerBoarding(Request $request)
+    public function createOwnerBoarding(Request $request)
     {   
         // dd($request); 
         $validation = $request->validate([
@@ -129,8 +133,9 @@ class BoardingController extends Controller
             'type' => ['required'],
             'rooms' => ['required','numeric','min:1'],
             'price' => ['required','numeric','min:1'],
+            'facility'=>['min:1'],
             'description' => ['required', 'max:200','min:5'],
-            'images' => ['max:3'],
+            'images' => ['max:5'],
         ]);
         
 
@@ -155,15 +160,16 @@ class BoardingController extends Controller
         ]);
 
 
-        if(isset($request['manager'])){
+        if(isset($request['manager']) && $request['manager']!== null){
             $ManagerBoardingNow = ManagerBoarding::create([
                 'owner_boarding_id' =>$OwnerBoardingNow->id,
-                'user_id'=>$request['manager']['id'],
+                'manager_user_id'=>$request['manager']['id'],
+                'boarding_id'=>$BoardingNow->id,
             ]);
         }   
         
         //FILES
-        if(isset($request['images'])){
+        if(($request->file('images') !== null)){
             foreach($request->file('images') as $image){
                 
                 $path = $image->getClientOriginalName();
@@ -172,7 +178,9 @@ class BoardingController extends Controller
 
                 $img = new BoardingImage();
 
-                $img->image = $image->storeAs('public/Boarding_House_Images', $path);
+                // $img->image = $image->storeAs('public/Boarding_House_Images', $path);
+                Storage::putFileAs('public/Boarding_House_Images',$image, $path);
+                $img->image = $path;
                 $img->boarding_id = $BoardingNow->id;
                 $img->save();
             }
@@ -181,33 +189,4 @@ class BoardingController extends Controller
         return redirect('/boardingOwner')->with('message', 'Success Adding new Boarding House');
     }
 
-    //Store a newly created resource in storage
-    public function store(Request $request)
-    {
-        //
-    }
-
-    //Display the specified resource.
-    public function show($id)
-    {
-        //
-    }
-
-    //Show the form for editing the specified resource
-    public function edit($id)
-    {
-        //
-    }
-
-    //Update the specified resource in storage.
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    //Remove the specified resource from storage
-    public function destroy($id)
-    {
-        //
-    }
 }
