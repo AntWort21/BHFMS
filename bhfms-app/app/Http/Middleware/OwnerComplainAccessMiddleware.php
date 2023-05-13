6ce88c0180feb6c27258df046616fd9fb5af9b27
AntWort21 <67149821+AntWort21@users.noreply.github.com>
2023-05-13 19:48:28 +0700

    fixed user_id not found

--- bhfms-app/app/Http/Middleware/OwnerComplainAccessMiddleware.php
+++ bhfms-app/app/Http/Middleware/OwnerComplainAccessMiddleware.php
@@ -19,12 +19,12 @@ class OwnerComplainAccessMiddleware
      */
     public function handle(Request $request, Closure $next)
     {
-        $ownerId = OwnerBoarding::where('boarding_id', $request->id)->first()->user_id;
+        $ownerId = OwnerBoarding::where('boarding_id', $request->id)->first()->user_id ?? -1;
         $managerId = ManagerBoarding::where('boarding_id', $request->id)->first()->user_id ?? -1;
         if(!Auth::user()->id){ //not logged in
             abort(404, 'Not Found');
         }
-        if (Auth::user()->user_role_id == 3 && Auth::user()->id !== $ownerId) { //case if owner
+        if (Auth::user()->user_role_id == 3 && (Auth::user()->id !== $ownerId || $ownerId == -1)) { //case if owner
             abort(404, 'Not Found');
         }
 
