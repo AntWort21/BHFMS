e4ee6f7268b9d12c092f8643cb7e2ba3b19cb05b
Aditya Winata <67149821+AntWort21@users.noreply.github.com>
2023-05-17 21:30:30 +0700

    added hidden to SElect your 'label' (#100)

--- bhfms-app/resources/js/Shared/AccountFormInput/FormSelectInput.vue
+++ bhfms-app/resources/js/Shared/AccountFormInput/FormSelectInput.vue
@@ -13,7 +13,7 @@ defineProps({
         :placeholder="labelName"
         class="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
     >
-        <option value="" disabled selected>Select your {{ labelName }}</option>
+        <option value="" disabled hidden selected>Select your {{ labelName }}</option>
         <option v-for="option in optionList" :value="option">{{ option }}</option>
     </select>
 </template>
