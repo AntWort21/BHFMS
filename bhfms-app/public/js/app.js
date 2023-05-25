cbdf32707c4202fc2aaee5b526ac6feecbb62979
AndreanIG <andrean.2000@gmail.com>
2023-05-25 11:17:22 +0700

    NPM RUN DEV

--- bhfms-app/public/js/app.js
+++ bhfms-app/public/js/app.js
@@ -23235,8 +23235,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
   name: 'UpdateTransactionType',
   props: {
-    transactionType: Object,
-    n: Object
+    transactionType: Object
   },
   setup: function setup(__props, _ref) {
     var expose = _ref.expose;
