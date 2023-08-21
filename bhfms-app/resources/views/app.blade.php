b627eabf19fa39137b22287a5c7456e266307339
AndreanIG <andrean.2000@gmail.com>
2023-08-21 14:22:35 +0700

    Updated so key is in ENV file

--- bhfms-app/resources/views/app.blade.php
+++ bhfms-app/resources/views/app.blade.php
@@ -7,7 +7,9 @@
     <script src="{{ mix('/js/app.js') }}" defer></script>
 
     <link href="{{ asset('/css/vue-multiselect.css') }}" rel="stylesheet">
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBn2AfcDJeTgCWaM8zY3nPlKxZEv2ce3lU&libraries=places&callback=Function.prototype"></script>
+    
+    <script src="https://maps.googleapis.com/maps/api/js?key={{ config('googleAPI.key')}}&libraries=places&callback=Function.prototype"></script>
+
     {{-- <meta name="csrf-token" content="{{ csrf_token() }}"> --}}
     @inertiaHead
   </head>
